791834a72f7f9600ed2b3769b28040cfcf41b06a
Jesusmaster <armandodejesus678@gmail.com>
2019-08-16 11:53:14 -0500

    script creador y servidores listos

---
+++ webpack/webpack.client.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const extract = require('mini-css-extract-plugin')
+const webpack = require('webpack');
+
+const config = {
+  entry: ["@babel/polyfill", path.resolve(__dirname, '../source/client.js')],
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: 'client.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: "/(node_modules)/",
+        options: {
+          presets: ['@babel/preset-env','@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [extract.loader, "css-loader?modules=true&localIdentName=[hash:base64:15]"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  plugins: [
+    new extract({
+      filename: "style.css"
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      BASEURL: JSON.stringify(process.env.NODE_ENV === 'production' ? 'https://platzi-blog.now.sh' : 'http://localhost:3000'),
+    })
+  ],
+  target: 'web',
+}
+
+module.exports = config;
---
+++ webpack/webpack.server.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const extract = require('mini-css-extract-plugin')
+const webpack = require('webpack');
+
+const config = {
+  entry: ["@babel/polyfill", path.resolve(__dirname, '../source/server.js')],
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: 'server.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: "/(node_modules)/",
+        options: {
+          presets: ['@babel/preset-env','@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [extract.loader, "css-loader?modules=true&localIdentName=[hash:base64:15]"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  plugins: [
+    new extract({
+      filename: "style.css"
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      BASEURL: JSON.stringify(process.env.NODE_ENV === 'production' ? 'https://platzi-blog.now.sh' : 'http://localhost:3000'),
+      STATICURL: JSON.stringify(process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8080')
+    })
+  ],
+  target: 'node',
+}
+
+
+module.exports = config;
